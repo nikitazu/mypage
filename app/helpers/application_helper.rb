a25aaa6469c595dfb8abe8e7518cab7d3ec0c2a0
Nikita B. Zuev <nikitazu@gmail.com>
2014-08-10 17:08:15 +0400

    Main menu links can be active

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,13 @@ module ApplicationHelper
     key = ['a','b','c'][rand(0..2)]
     t("quotes.#{key}")
   end
+  
+  def active_link_to(title, path, param={})
+    if param.has_key?(:class)
+      param[:class] += (current_page?(path) ? ' active' : '')
+    else
+      param[:class] = (current_page?(path) ? 'active' : '')
+    end
+    link_to title, path, param
+  end
 end
