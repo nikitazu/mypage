4bdfc89a6cf4f76eb95a7cea5d0c649cd7c459d9
Nikita B. Zuev <nikitazu@gmail.com>
2014-08-10 16:53:01 +0400

    User can now switch localizations
    Application detects user language from browser client data.
    Localization parameter is passed through the urls.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
-  resources :works
-
-  resources :ideas
-
+  
+  scope "(:locale)", locale: /ru|en/ do
+    resources :works
+    resources :ideas
+  end
+  
+  get '/:locale' => 'welcome#index'
   root 'welcome#index'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
