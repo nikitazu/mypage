eb242a70fc5f1364a4a88b9f5790735bce30d56b
Nikita B. Zuev <nikitazu@gmail.com>
2014-08-12 22:25:54 +0400

    Ideas easier to navigate UI
    List clicking leads to edit mode
    From edit mode you can delete
    After edit or create you are back in the list

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -28,7 +28,7 @@ class IdeasController < ApplicationController
 
     respond_to do |format|
       if @idea.save
-        format.html { redirect_to @idea, notice: 'Idea was successfully created.' }
+        format.html { redirect_to ideas_path, notice: 'Idea was successfully created.' }
         format.json { render :show, status: :created, location: @idea }
       else
         format.html { render :new }
@@ -42,7 +42,7 @@ class IdeasController < ApplicationController
   def update
     respond_to do |format|
       if @idea.update(idea_params)
-        format.html { redirect_to @idea, notice: 'Idea was successfully updated.' }
+        format.html { redirect_to ideas_path, notice: 'Idea was successfully updated.' }
         format.json { render :show, status: :ok, location: @idea }
       else
         format.html { render :edit }
